59d8eb705855af68a641c82ee56864af4f839db5
Luis García <105936384+lglabs@users.noreply.github.com>
2023-07-02 23:20:21 +0200

    Update EmptyEvent.java

--- src/main/java/com/labs/lg/pentagon/common/domain/event/EmptyEvent.java
+++ src/main/java/com/labs/lg/pentagon/common/domain/event/EmptyEvent.java
@@ -1,7 +1,6 @@
 package com.labs.lg.pentagon.common.domain.event;
 
 /**
- * The objective is disabled fire() method,
  * It will be used in the last step SAGA pattern, so, is unnecessary producer more events...
  * remember only, in the final step of SAGA.
  * <p>
@@ -12,10 +11,5 @@ public final  class EmptyEvent implements DomainEvent<Void>{
   public static final EmptyEvent INSTANCE = new EmptyEvent();
 
   private EmptyEvent() {
-  }
-
-  @Override
-  public void fire() {
-
   }
 }
